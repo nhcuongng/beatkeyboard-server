ebd921fff685988acc7b2631a9469cf623edaf8d
nhcuongng <cuong.nguyenhuu1997@gmail.com>
2018-10-27 04:27:06 +0700

    update

--- config/url.js
+++ config/url.js
@@ -1,4 +1,4 @@
 module.exports = {
-    'urlClient': 'https://beatkeyboard.herokuapp.com/',
-    'urlServer': 'https://beatkeyboard-server.herokuapp.com/'
+    'urlClient': 'https://beatkeyboard.herokuapp.com',
+    'urlServer': 'https://beatkeyboard-server.herokuapp.com'
 }
