d70f0a6799e352de465f50cedbb3c6bdf7d128e4
nhcuongng <cuong.nguyenhuu1997@gmail.com>
2018-10-27 04:43:47 +0700

    update

--- config/auth.js
+++ config/auth.js
@@ -3,7 +3,7 @@ module.exports = {
     'facebookAuth' : {
         'clientID'        : '2253490058199856', // your App ID
         'clientSecret'    : 'c7c3d3ac930671cb39bf459a35cde8f9', // your App Secret
-        'callbackURL'     : 'https://beatkeyboard-server.herokuapp.com/auth/callback',
+        'callbackURL'     : 'https://beatkeyboard-server.herokuapp.com/api/auth/callback',
         'profileURL': 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email',
         'profileFields'   : ['id', 'email', 'name','photos'] // For requesting permissions from Facebook API
     },
