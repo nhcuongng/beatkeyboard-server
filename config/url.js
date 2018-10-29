d71a3e68d5a9eb98baf7135b6095caa06f0c32da
nhcuongng <cuong.nguyenhuu1997@gmail.com>
2018-10-29 13:21:38 +0700

    run on local host

--- config/url.js
+++ config/url.js
@@ -1,4 +1,9 @@
 module.exports = {
-    'urlClient': 'https://beatkeyboard.herokuapp.com',
-    'urlServer': 'https://beatkeyboard-server.herokuapp.com'
+    // on heroku
+    // 'urlClient': 'https://beatkeyboard.herokuapp.com',
+    // 'urlServer': 'https://beatkeyboard-server.herokuapp.com'
+
+    // on localhost
+    urlClient: 'http://localhost:3000',
+    urlServer: 'http://localhost:8080'
 }
