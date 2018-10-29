3bbb119f22925f9be52eb38f31048753f722b1c9
nhcuongng <cuong.nguyenhuu1997@gmail.com>
2018-10-29 23:10:44 +0700

    change to common app

--- config/auth.js
+++ config/auth.js
@@ -1,8 +1,8 @@
 
 module.exports = {
     'facebookAuth' : {
-        'clientID'        : '2253490058199856', // your App ID
-        'clientSecret'    : 'c7c3d3ac930671cb39bf459a35cde8f9', // your App Secret
+        'clientID'        : '734238596944122', // your App ID
+        'clientSecret'    : '52aeb2d122fe01f62cc9f4b5ec13944a', // your App Secret
         'callbackURL'     : 'http://localhost:8080/api/auth/facebook/callback',// on heroku: https://beatkeyboard-server.herokuapp.com 
         'profileURL': 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email',
         'profileFields'   : ['id', 'email', 'name','photos'] // For requesting permissions from Facebook API
