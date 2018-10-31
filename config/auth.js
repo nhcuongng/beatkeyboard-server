6905f1b24520aee847f75e53caad6aabd8a922fb
nhcuongng <cuong.nguyenhuu1997@gmail.com>
2018-11-01 06:00:29 +0700

    deploy_v1

--- config/auth.js
+++ config/auth.js
@@ -1,9 +1,9 @@
 
 module.exports = {
     'facebookAuth' : {
-        'clientID'        : '504950676651937', // your App ID
-        'clientSecret'    : '603904ee9f534deccfced9c0cd202ce6', // your App Secret
-        'callbackURL'     : 'http://localhost:8080/api/auth/facebook/callback',// on heroku: https://beatkeyboard-server.herokuapp.com 
+        'clientID'        : '2253490058199856', // your App ID
+        'clientSecret'    : 'c7c3d3ac930671cb39bf459a35cde8f9', // your App Secret
+        'callbackURL'     : 'https://beatkeyboard-server.herokuapp.com/api/auth/facebook/callback',// on heroku: https://beatkeyboard-server.herokuapp.com 
         'profileURL': 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email',
         'profileFields'   : ['id', 'email', 'name','photos'] // For requesting permissions from Facebook API
     },
--- server.js
+++ server.js
@@ -1,47 +1,83 @@
-var express  = require('express');
-var app      = express();
-// var io = require('socket.io')();
+var app  = require('express')();
+var server = require('http').Server(app)
+const io = require('socket.io')(server);
 const port = process.env.PORT || 8080;
 var mongoose = require('mongoose');
 var passport = require('passport');
 var cors = require('cors')
-
 var cookieParser = require('cookie-parser');
-var bodyParser   = require('body-parser');
 var session      = require('express-session');
+var bodyParser = require('body-parser')
+
 
 var configDB = require('./config/database.js');
 
-// listen socket
-// io.on('connection', (client) => {
-//     client.on('subscribeToTimer', (interval) => {
-//       console.log('client is subscribing to timer with interval ', interval);
-//       setInterval(() => {
-//         client.emit('timer', new Date());
-//       }, interval);
-//     });
-//     console.log(client.id)
-//   });
-mongoose.connect(configDB.url); // connect to our database
-// allow cors with credential
-app.use(cors({ origin:"http://localhost:3000", credentials:true }))
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
-app.use(cookieParser()); // read cookies (needed for auth)
-require('./config/passport')(passport); // pass passport for configuration
+
+var countRoom = 1;
+var count= 1;
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.use(session({
     secret: 'ilovescotchscotchyscotchscotch', // session secret
     resave: false,
     saveUninitialized: true
 }));
+
+function bot(socket,room) {
+  console.log('đây là bot game')
+  io.engine.generateId = (req) => {
+    return "daylabotgamebeatkeys"
+  }
+  socket.join(room)
+  const characters = ["a","b", "c", "d","e","f", "g", "h","i","j", "k", "l","m","n","o","p", "q", "r","s","t", "v", "x","y","z", 
+                      "1", "2","3","4", "5", "6","7","8", "9", "10"
+                      ,"[","]", "{", "}","<",">", "?", "=","+","_", "\\", "/",":","", "'", " ","`","~"]
+  
+    
+  socket.on('user1-send-to-server',() => {
+      socket.broadcast.to(room).emit("server-send-to-user2",characters[Math.floor(Math.random() * characters.length)+1]) // send to other client in room
+    })
+
+}
+
+
+
+io.on('connection', (socket) => {
+  var room = '#Battleroyal ' + count;
+  // // Join to room and limit 2 members in one room (rooms is difference)
+    if(!socket.connected)  bot(socket,room)
+    else {
+      socket.join(room, () => { 
+        let rooms = Object.keys(socket.rooms);
+        console.log(rooms)
+        countRoom++
+      })
+      if(countRoom % 4 === 0){
+        count++
+      }
+      socket.on('user1-send-to-server',(data) => {
+        socket.broadcast.to(room).emit("server-send-to-user2",data) // send to other client in room
+      })
+    }
+  })
+
+
+
+mongoose.connect(configDB.url); // connect to our database
+// allow cors with credential
+app.use(cors({ origin:"https://beatkeyboard.herokuapp.com", credentials:true }))
+app.use(cookieParser()); // read cookies (needed for auth)
+require('./config/passport')(passport); // pass passport for configuration
+
+
+
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 
 require('./app/routes.js')(app); // load our routes and pass in our app and fully configured passport
 require('./app/router.js')(app);
 // launch ======================================================================
-app.listen(port, (err) =>{
-    if(err) console.log(err)
-    else console.log("Listen at " + port)
-});
+server.listen(port);
+console.log('listening on port ', port);
